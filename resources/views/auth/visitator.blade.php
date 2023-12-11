8a5b57ac9bc50274b4031b6a9f05533f68767819
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2023-12-11 10:05:17 -0600

    Cambio de diseño checkin
    Reajuste de checkin diseño

--- resources/views/auth/visitator.blade.php
+++ resources/views/auth/visitator.blade.php
@@ -14,28 +14,31 @@
 
 @section('subcontent')
 <main class="py-5">
-    <div class="container">
-        @include('alerta')
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        <form method="POST" id="envio" action="{{ route('api.registrarVisita') }}">
-                            @csrf
-                            <div class="form-group row justify-content-center"> 
-                                <label  class="col-md-3 col-form-label text-md-right text-lg">{{ __('Telefono') }}</label> 
-                                <div class="col-md-6">
-                                    <input id="codigo" class="form-control form-control-lg" name="codigo"> 
+    <div class="intro-y box p-5" style="margin: 0 20% 0 20%">
+        <div class="container">
+            @include('alerta')
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="POST" id="envio" action="{{ route('api.registrarVisita') }}">
+                                @csrf
+                                <div class="form-group row justify-content-center"> 
+                                    <label  class="col-md-3 col-form-label text-md-right text-lg">{{ __('Check-In Visitante') }}</label> 
+                                    <div class="col-md-6">
+                                        <input id="codigo" class="form-control form-control-lg" name="codigo"> 
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row mb-0">
-                                <div class="col-md-9 offset-md-3">
-                                    <button type="submit" class="btn btn-primary btn-block center"> 
-                                        {{ __('Registrar visita') }}
-                                    </button>
+                                <div class="form-group row mb-0">
+                                    <br>
+                                    <div class="col-md-9 offset-md-3">
+                                        <button type="submit" class="btn btn-primary btn-block center"> 
+                                            {{ __('Registrar visita') }}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
