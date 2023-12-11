fa25a7f7e249ab6c15609c885d06a5908a57a3a4
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2023-12-11 09:22:03 -0600

    Mensaje de error login
    Se muestra un error al introducir credenciales incorrectas

--- storage/framework/views/742de7467128764c8d843927ebf65a8c01fad27a.php
+++ storage/framework/views/742de7467128764c8d843927ebf65a8c01fad27a.php
@@ -10,7 +10,7 @@
 
     <div class="container">
         
-                
+              
         <div class="w-full min-h-screen p-5 md:p-20 flex items-center justify-center">
             <div class="w-96 intro-y">
 
@@ -18,7 +18,11 @@
                 <a href="<?php echo e(route('landing')); ?>"><img class="mx-auto my-auto" alt="Inventores" width="200px" height="150px" src="<?php echo e(asset('build/assets/logosinventores/InventoresBannerHDBlueBorder2.png')); ?>"></a>
                 
                 <div class="box px-5 py-8 mt-10 max-w-[450px] relative before:content-[''] before:z-[-1] before:w-[95%] before:h-full before:bg-slate-200 before:border before:border-slate-200 before:-mt-5 before:absolute before:rounded-lg before:mx-auto before:inset-x-0 before:dark:bg-darkmode-600/70 before:dark:border-darkmode-500/60">
-
+                    <?php if(session('FAIL')): ?>
+                        <div class="intro-y col-span-12 lg:col-span-6">
+                            <h6 class="alert alert-danger"><?php echo e(session('FAIL')); ?></h6>
+                        </div>
+                    <?php endif; ?>
                     <div class="container pt-3">
                     <form method="POST" id="login-form">
                         <?php echo csrf_field(); ?>
