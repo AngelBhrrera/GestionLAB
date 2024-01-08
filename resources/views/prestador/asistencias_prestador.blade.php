3f93c9356c9867259981ec344dc23ecc24f0f092
BHrrera <angel0cero0@gmail.com>
2024-01-08 10:24:37 -0600

    Caelndar 8/1/24
    Sin implementar

--- resources/views/prestador/asistencias_prestador.blade.php
+++ resources/views/prestador/asistencias_prestador.blade.php
@@ -1,4 +1,35 @@
 @extends('layouts/prestador-layout')
+    
+@section('subhead')
+
+    <style>
+    #container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh; /* Altura total de la ventana */
+    }
+
+    #calendar {
+        width: 300px;
+        height: 300px;
+    }
+    </style>
+
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+
+@endsection
 
 @section('breadcrumb')
     <nav aria-label="breadcrumb" class="-intro-x hidden xl:flex">
@@ -11,5 +42,10 @@
 
 @section('subcontent')
 
+<body>
+    <div id='container'>
+        <div id='calendar'  style='width: 300px; height: 300px; margin: 0 auto;'></div>
+    </div>
+</body>
 
 @endsection
