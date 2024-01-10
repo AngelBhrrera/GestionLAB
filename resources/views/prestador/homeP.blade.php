c5bda2a2939e392e0310d5193c20e121b776015c
BHrrera <angel0cero0@gmail.com>
2024-01-10 12:25:32 -0600

    SIUUUUU
    10/1/24
    
    Arreglos de admin

--- resources/views/prestador/homeP.blade.php
+++ resources/views/prestador/homeP.blade.php
@@ -69,179 +69,31 @@
 @endsection
 
 @section('script')
-    <script type="text/javascript">
-    var tabledata = [{
-                    playerid: 1,
-                    playername: "Virat Kohli",
-                    price: "40",
-                    team: "RCB",
-                    joiningdate: "01/01/2020"
-                }, {
-                    playerid: 2,
-                    playername: "Rohit Sharma",
-                    price: "150",
-                    team: "MI",
-                    joiningdate: "02/01/2020"
-                }, {
-                    playerid: 3,
-                    playername: "MS Dhoni",
-                    price: "15",
-                    team: "CSK",
-                    joiningdate: "03/01/2020"
-                }, {
-                    playerid: 4,
-                    playername: "Shreyas Iyer",
-                    price: "7",
-                    team: "RCB",
-                    joiningdate: "04/01/2020"
-                }, {
-                    playerid: 5,
-                    playername: "KL Rahul",
-                    price: "11",
-                    team: "KXIP",
-                    joiningdate: "05/01/2020"
-                }, {
-                    playerid: 6,
-                    playername: "Dinesh Karthik",
-                    price: "7",
-                    team: "KKR",
-                    joiningdate: "06/01/2020"
-                }, {
-                    playerid: 7,
-                    playername: "Steve Smith",
-                    price: "12",
-                    team: "RR",
-                    joiningdate: "07/01/2020"
-                }, {
-                    playerid: 8,
-                    playername: "David Warner",
-                    price: "120",
-                    team: "SRH",
-                    joiningdate: "08/01/2020"
-                }, {
-                    playerid: 9,
-                    playername: "Kane Williamson",
-                    price: "3",
-                    team: "SRH",
-                    joiningdate: "09/01/2020"
-                }, {
-                    playerid: 10,
-                    playername: "Jofra Archer",
-                    price: "7",
-                    team: "RR",
-                    joiningdate: "10/01/2020"
-                }, {
-                    playerid: 11,
-                    playername: "Andre Russell",
-                    price: "9",
-                    team: "KKR",
-                    joiningdate: "11/01/2020"
-                }, {
-                    playerid: 12,
-                    playername: "Chris Gayle",
-                    price: "2",
-                    team: "KXIP",
-                    joiningdate: "12/01/2020"
-                },
-                {
-                    playerid: 1,
-                    playername: "Virat Kohli",
-                    price: "40",
-                    team: "RCB",
-                    joiningdate: "01/01/2020"
-                }, {
-                    playerid: 2,
-                    playername: "Rohit Sharma",
-                    price: "150",
-                    team: "MI",
-                    joiningdate: "02/01/2020"
-                }, {
-                    playerid: 3,
-                    playername: "MS Dhoni",
-                    price: "15",
-                    team: "CSK",
-                    joiningdate: "03/01/2020"
-                }, {
-                    playerid: 4,
-                    playername: "Shreyas Iyer",
-                    price: "7",
-                    team: "RCB",
-                    joiningdate: "04/01/2020"
-                }, {
-                    playerid: 5,
-                    playername: "KL Rahul",
-                    price: "11",
-                    team: "KXIP",
-                    joiningdate: "05/01/2020"
-                }, {
-                    playerid: 6,
-                    playername: "Dinesh Karthik",
-                    price: "7",
-                    team: "KKR",
-                    joiningdate: "06/01/2020"
-                }, {
-                    playerid: 7,
-                    playername: "Steve Smith",
-                    price: "12",
-                    team: "RR",
-                    joiningdate: "07/01/2020"
-                }, {
-                    playerid: 8,
-                    playername: "David Warner",
-                    price: "120",
-                    team: "SRH",
-                    joiningdate: "08/01/2020"
-                }, {
-                    playerid: 9,
-                    playername: "Kane Williamson",
-                    price: "3",
-                    team: "SRH",
-                    joiningdate: "09/01/2020"
-                }, {
-                    playerid: 10,
-                    playername: "Jofra Archer",
-                    price: "7",
-                    team: "RR",
-                    joiningdate: "10/01/2020"
-                }, {
-                    playerid: 11,
-                    playername: "Andre Russell",
-                    price: "9",
-                    team: "KKR",
-                    joiningdate: "11/01/2020"
-                }, {
-                    playerid: 12,
-                    playername: "Chris Gayle",
-                    price: "2",
-                    team: "KXIP",
-                    joiningdate: "12/01/2020"
-                },
 
-            ];
+    <script type="text/javascript">
 
             var table = new Tabulator("#players", {
                 height: 225,
-                data: tabledata,
+
                 layout: "fitColumns",
                 pagination: "local",
                 paginationSize: 8,
                 tooltips: true,
                 columns: [{
-                        title: "Player Name",
+                        title: "Nombre",
                         field: "playername",
                         sorter: "string",
                         width: 150,
                         headerFilter: "input"
                     }, {
-                        title: "Player Price",
+                        title: "Codigo",
                         field: "price",
                         sorter: "number",
                         hozAlign: "left",
                         formatter: "progress",
                     },
-
                     {
-                        title: "Team",
+                        title: "Sede",
                         field: "team",
                         sorter: "string",
                         hozAlign: "center",
@@ -253,7 +105,7 @@
                             "KKR": "KKR",
                         }
                     }, {
-                        title: "Joining Date",
+                        title: "Fecha",
                         field: "joiningdate",
                         sorter: "date",
                         hozAlign: "center"
@@ -262,6 +114,19 @@
                 //rowClick: function(e, row) {
                 //    alert("Row " + row.getData().playerid + " Clicked!!!!");
                 //},
+            });
+
+
+            $.ajax({
+                url: 'users.php', 
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    table.setData(data);
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
             });
             
     </script>
