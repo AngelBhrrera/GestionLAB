5a944b9a568aa55161855615c4db27ae3ae55b78
Carlos Chavoya <114368983+Chavoya99@users.noreply.github.com>
2024-01-29 09:59:49 -0600

    Implementación de asistencias en calendario

--- resources/views/prestador/horario_prestador.blade.php
+++ resources/views/prestador/horario_prestador.blade.php
@@ -1,32 +1,64 @@
+
 @extends('layouts/prestador-layout')
 
+
 @section('subhead')
-
-    <script>
-
-      document.addEventListener('DOMContentLoaded', function() {
-          var calendarEl = document.getElementById('calendar');
-          var calendar = new FullCalendar.Calendar(calendarEl, {
-              initialView: 'dayGridMonth',
-              initialDate: new Date()  // Establecer la fecha actual como la fecha inicial
-          });
-          calendar.render();
-      });
-
-    </script>
-
 
 @endsection
 
 @section('breadcrumb')
-    <li class="breadcrumb-item"><a href="{{route('homeP')}}">{{$userRol=ucfirst(Auth::user()->tipo)}}</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Horario prestador</li>
+    <nav aria-label="breadcrumb" class="-intro-x hidden xl:flex">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('homeP')}}">Prestador</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Horario prestador</li>
+        </ol>
+    </nav>
 @endsection
 
 
 @section('subcontent')
+
+
     <div id='container'>
         <div id='calendar'  style='width: 800px; height: 1150px;'></div>
     </div>
+    <div id="asistencias" data-asistencias="{{json_encode($asistencias)}}"></div>
+
+
 @endsection
+
+<script src="{{asset('build/assets/js/calendar.global.min.js')}}"></script>
+
+<script>
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    
+    var asistencias = document.getElementById("asistencias").getAttribute('data-asistencias');
+    var array = JSON.parse(asistencias);
+    var a=[];
+    
+    //Asistencias
+    array.forEach(function(elemento) {
+          console.log(elemento.fecha);
+          a.push({
+          id: 'a',
+          title: 'Asistencia',
+          start: elemento.fecha,
+          end: elemento.fecha,
+          backgroundColor: "#00FFF0",
+          display: "background"
+        });
+    });
+    
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialDate: Date.now(),
+      events: [
+          ...a
+        ],
+    });
+    calendar.render();
+  });
+
+</script>
 
