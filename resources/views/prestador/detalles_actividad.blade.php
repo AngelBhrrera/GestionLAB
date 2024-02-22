fff26f1447c4d17ab18075c8b82600138547d8fe
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-02-22 12:07:23 -0600

    actividades_detalle
    detalles

--- resources/views/prestador/detalles_actividad.blade.php
+++ resources/views/prestador/detalles_actividad.blade.php
@@ -10,39 +10,42 @@
             <li class="breadcrumb-item active" aria-current="page">Detalle Actividad</li>
         </ol>
     </nav>
+    <link rel="stylesheet" href="{{asset('build/assets/css/detalles_actividad.css')}}">
 @endsection
 
 @section('subcontent')
-<div class="container">
-    <h1 class="text-center">Detalle Actividad</h1>
+<div class="container" id="contenedor-actividades">
+    <h1 class="text-center" id="detalle_titulo">Detalle Actividad</h1>
     <div class="text-center mx-auto" style="padding-left: 1.5px;" id="players"></div>
-    <ul>
-            <li><strong>Categoría:</strong> {{ $detalle->nombre_categoria }}</li>
-            <li><strong>Subcategoría:</strong> {{ $detalle->nombre_subcategoria }}</li>
-            <li><strong>Título:</strong> {{ $detalle->titulo }}</li>
-            <li><strong>Tipo:</strong> {{ $detalle->tipo }}</li>
-            <li><strong>Recursos:</strong>
-                <ul>
-                    @php
-                        $recursos = explode(',', $detalle->recursos);
-                    @endphp
-                    @foreach ($recursos as $recurso)
-                        <li><input type='checkbox'> {{ $recurso }}</li>
-                    @endforeach
-                </ul>
-            </li>
-            <li><strong>Descripción:</strong> {{ $detalle->descripcion }}</li>
-            <li><strong>Objetivos:</strong>
-                <ul>
-                    @php
-                        $objetivos = explode(',', $detalle->objetivos);
-                    @endphp
-                    @foreach ($objetivos as $objetivo)
-                        <li><input type='checkbox'> {{ $objetivo }}</li>
-                    @endforeach
-                </ul>
-            </li>
-        </ul>
+    <div id="actividad">
+        <ul>
+                <h1 id="titulo_actividad">{{$detalle->titulo}}</h1>
+                <li id="categorias"><strong>Categoría:</strong> {{ $detalle->nombre_categoria }}</li>
+                <li id="categorias"><strong>Subcategoría:</strong> {{ $detalle->nombre_subcategoria }}</li>
+                <li id="categorias"><strong>Tipo:</strong> {{ $detalle->tipo }}</li>
+                <li id="categorias"><strong>Descripción:</strong> {{ $detalle->descripcion }}</li>
+                <li id="categorias"><strong>Objetivos:</strong>
+                    <ul>
+                        @php
+                            $objetivos = explode(',', $detalle->objetivos);
+                        @endphp
+                        @foreach ($objetivos as $objetivo)
+                            <li id="recursos"><input type='checkbox'> {{ $objetivo }}</li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li id="recursos"><strong>Recursos:</strong>
+                    <ul>
+                        @php
+                            $recursos = explode(',', $detalle->recursos);
+                        @endphp
+                        @foreach ($recursos as $recurso)
+                            <li id="recursos"><input type='checkbox'> {{ $recurso }}</li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+    </div>
 </div>
 @endsection
 
