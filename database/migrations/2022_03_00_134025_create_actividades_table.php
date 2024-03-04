1d3a3f2c291a12196946650f74b0c8c62c6c243d
BHrrera <angel0cero0@gmail.com>
2024-03-03 22:49:11 -0600

    Update 2022_03_00_134025_create_actividades_table.php

--- database/migrations/2022_03_00_134025_create_actividades_table.php
+++ database/migrations/2022_03_00_134025_create_actividades_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateActividadesTable extends Migration
 {
@@ -32,9 +33,9 @@ class CreateActividadesTable extends Migration
             "TEC" => 20,
             "exp_ref" => 5,
             "tipo" => 1,
-            "recursos" => "A,B,C,D",
-            "descripcion" => "ejemplo",
-            "objetivos"=> "1,2,3,4",
+            "recursos" => "software de preparacion para impresion,computadora,memoria microsd,adaptador,impresora, filamento",
+            "descripcion" => "Brindar el servicio de impresion de formas 3D mediante el uso de las impresoras del laboratorio",
+            "objetivos"=> "brindar un servicio eficaz, no hacer gasto inadecuado del material, realizar la impresion tomando en cuenta el mantenimiento correspondiente a la impresora",
         ]);
         DB::table('actividades')->insert([
             "titulo" =>"Mantenimiento Impresora 3D Laboratorio",
@@ -43,9 +44,9 @@ class CreateActividadesTable extends Migration
             "TEC" => 90,
             "exp_ref" => 10,
             "tipo" => 1,
-            "recursos" => "A,B,C,D",
-            "descripcion" => "ejemplo",
-            "objetivos"=> "1,2,3,4",
+            "recursos" => "herramientas de mantenimiento",
+            "descripcion" => "Llevar a cabo de forma periodica una revision exhaustiva del estado actual del equipo utilizado en el Laboratorio para la elaboracion de impresiones 3D",
+            "objetivos"=> "mantener un orden del uso que se le da a las impresiones, brindar un mantenimiento preventivo o correctivo en caso de necesitarse",
         ]);
 
     }
