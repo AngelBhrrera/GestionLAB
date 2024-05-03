09e7cde8a3cec7b046bc60ff586783ef9dddb966
BHrrera <angel0cero0@gmail.com>
2024-05-02 22:58:53 -0600

    Ajuste

--- public/decisionTreeModel.py
+++ public/decisionTreeModel.py
@@ -7,19 +7,18 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 import pandas as pd
 import numpy as np
 import json
-'''
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt 
-'''
+
 #SS MODULAR RENDIMIENTO CALCULADOR
 df2 = pd.read_csv('registro_acts.csv')
 
-X = df2.drop(columns=['resultado','id_prestador','id_actividad']) 
-y = df2['resultado'] 
+X = df2.drop(columns=['Resultado']) 
+y = df2['Resultado'] 
 X = pd.get_dummies(X)
-
+'''
 clf = DecisionTreeClassifier()
 clf.fit(X, y)
  
@@ -49,11 +48,11 @@ resultados_dict = df_resultados_sorted.to_dict(orient='records')
 resultados_json = json.dumps(resultados_dict)
 
 # Imprimir la cadena JSON para verificar
-print(resultados_json)
-
+#print(resultados_json)
 '''
-# Dividir el conjunto de datos en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+random_state = np.random.randint(1, 251)
+print(random_state)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
 # Crear el modelo de árbol de decisión
 clf = DecisionTreeClassifier()
 # Entrenar el modelo con los datos de entrenamiento
@@ -63,21 +62,15 @@ y_pred = clf.predict(X_test)
 # Calcular la precisión del modelo
 accuracy = accuracy_score(y_test, y_pred)
 print("Precisión:", accuracy)
-# Obtener la matriz de confusión
-conf_matrix = confusion_matrix(y_test, y_pred)
-class_names = np.unique(y)
 # Orden deseado de las etiquetas
-orden_etiquetas = ['Fuera de rango', 'Deficiente', 'Regular', 'Aceptable', 'Bueno', 'Excelente']
-# Crear un mapeo de las etiquetas actuales a su índice deseado
-mapeo_etiquetas = {etiqueta: indice for indice, etiqueta in enumerate(orden_etiquetas)}
-# Reordenar la matriz de confusión y las etiquetas
-conf_matrix_reordenada = conf_matrix[np.argsort(list(map(mapeo_etiquetas.get, class_names)))]
-class_names_reordenados = np.array(orden_etiquetas)
+orden_etiquetas = ['Deficiente', 'Regular', 'Aceptable', 'Bueno', 'Excelente']
+# Obtener la matriz de confusión
+conf_matrix = confusion_matrix(y_test, y_pred, labels=orden_etiquetas)
+
 # Visualizar la matriz de confusión reordenada
 plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix_reordenada, annot=True, fmt='d', cmap='Blues', xticklabels=class_names_reordenados, yticklabels=class_names_reordenados)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=orden_etiquetas, yticklabels=orden_etiquetas)
 plt.xlabel('Clases Predichas')
 plt.ylabel('Clases Reales')
 plt.title('Matriz de Confusión')
 plt.show()
-'''
